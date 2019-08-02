767b4bd8260153ed5e1d5418bccecc3ece197128
Rick Brown <ricky.brown.00@gmail.com>
2019-08-02 16:41:38 +0100

    Converted Headline to a funtional component

--- src/Components/Headline.js
+++ src/Components/Headline.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class Headline extends React.Component {
-  render() {
-    const { header, desc, postedBy } = this.props
-    if (!header) return null
+const Headline = ({ header, desc, postedBy }) => {
+  if (!header) return null
 
-    return (
-      <div data-test='headline-component'>
-        <h1 data-test='headline-header'>{header}</h1>
-        <p data-test='headline-desc'> {desc}</p>
-      </div>
-    )
-  }
+  return (
+    <div data-test='headline-component'>
+      <h1 data-test='headline-header'>{header}</h1>
+      <p data-test='headline-desc'> {desc}</p>
+    </div>
+  )
 }
 
 Headline.propTypes = {
